0f4e41d09d3c8efd19b23d30748f935c8ec94ae2
Your Name <you@example.com>
2021-02-02 21:30:41 +0100

    added request.POST in order to add from the web

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 
 def home(request):
@@ -9,5 +10,13 @@ def home(request):
 
 def task(request):
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/tasks/')
+
+    context = {'tasks': tasks,
+               'form': form}
     return render(request, 'tasks/activity.html', context)
