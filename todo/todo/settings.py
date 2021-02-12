d4e684f2d932b1459f50003495727237aa7f2d3d
Your Name <tomposki@gmail.com>
2021-02-12 20:32:06 +0100

    UPDATED SWTTINGS

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'lu$lccb$pq2c(*33va&j-ru7*%j*_)j$8rw!25g&xuc#jrmcjp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysuperbtodoapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
